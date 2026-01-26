bedb5b3d60371aa722259ff52fb00b651fe91389
Muhammed Fatih Uğurlu <muhammedfatihugurlu10@gmail.com>
2026-01-26 19:03:36 +0300

    1

--- fatih1453.py
+++ fatih1453.py
@@ -34,7 +34,7 @@ with st.sidebar:
             st.session_state.arsiv[f"{tarih} | {ozet}"] = list(st.session_state.messages)
             st.success("Arşive eklendi!")
         else:
-            st.warning("Boş sohbeti kaydedememd")
+            st.warning("Boş sohbeti kaydedemem")
 
     st.divider()
     st.subheader("Eski Kayıtlar")
