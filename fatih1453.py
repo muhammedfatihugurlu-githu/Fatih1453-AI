70060abe340f089a3b19672c228536573bb64147
Muhammed Fatih Uğurlu <muhammedfatihugurlu10@gmail.com>
2026-09-10 21:58:39 +0300

    1

--- fatih1453.py
+++ fatih1453.py
@@ -115,7 +115,7 @@ if prompt:
             
             else:
                 completion = client.chat.completions.create(
-                    model="llama-3.3-70b-specdec",
+                    model="llama-3.1-8b-instant",
                     messages=[{"role": "system", "content": FATIH_PROMPT}] + st.session_state.messages,
                     stream=True
                 )
